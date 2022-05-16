b8218bc0f1fc936aad4a9c65f202629d2f0578d9
jianglei <1021124094@qq.com>
2022-05-16 19:57:31 +0800

    ✨ feat:实现 effect & reactive & 依赖收集 & 触发依赖

---
+++ src/reactitity/effect.ts
@@ -0,0 +1,41 @@
+class RectiveEffect {
+  private _fn: () => void
+  constructor (fn) {
+    this._fn = fn
+  }
+  run () {
+    activeEffect = this
+    this._fn()
+  }
+}
+
+const targetMap = new Map()
+
+export function track (target, key) {
+  let depsMap = targetMap.get(target)
+  if (!depsMap) {
+    depsMap = new Map()
+    targetMap.set(target, depsMap)
+  }
+
+  let dep = depsMap.get(key)
+  if (!dep) {
+    dep = new Set()
+    depsMap.set(key, dep)
+  }
+  dep.add(activeEffect)
+}
+
+export function trigger (target, key) {
+  const depsMap = targetMap.get(target)
+  let dep = depsMap.get(key)
+  for (let effect of dep) {
+    effect.run()
+  }
+}
+
+let activeEffect
+export function effect (fn) {
+  const _effect = new RectiveEffect(fn)
+  _effect.run()
+}
